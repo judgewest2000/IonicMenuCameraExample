010c551104285e210a40b3450636f7034d2afa2a
James West <james.west@westdarley.co.uk>
2017-10-11 21:16:52 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { CameraOptions, Camera } from '@ionic-native/camera';
 
 @Component({
   templateUrl: 'app.html'
@@ -14,9 +15,9 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any }>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, private camera: Camera) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -40,5 +41,42 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+
+  avatar = 'https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
+
+  private cameraExecute(options: CameraOptions) {
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.avatar = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+
+    });
+  }
+
+  choosePhoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }
+
+    this.cameraExecute(options);
+
+  }
+
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.CAMERA
+    }
+
+    this.cameraExecute(options);
+
   }
 }
